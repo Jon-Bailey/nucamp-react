5448e88f50bb0bfbb8e3e2298cbfc0f686db0c8b
Jon Bailey <jonbailey788@gmail.com>
2020-10-23 21:14:56 -0500

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,14 +7,20 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             return(
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(comment =>{
+                    {comments.map(comment => {
                     return (
-                    <div key={comment.id}>{comment.text} <br/> {comment.author} <br/>
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
+                    <div key={comment.id}> 
+                        <p>{comment.text} <br/> 
+                        -- {comment.author}, 
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                    </p>
+                    </div>
                     )
                     })}
-                    </div>)
-        }
+                    
+                    </div>
+                    );
+        };
         return<div/>;
     }
 
@@ -38,7 +44,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             <div className="container">
                 <div className="row">
                 <RenderCampsite campsite={props.campsite} />
-                <RenderComments comments={props.campsite.comments} />
+                <RenderComments comments={props.comments} />
                 </div> 
             </div>
             );
